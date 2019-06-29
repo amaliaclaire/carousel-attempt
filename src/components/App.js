0e78bbe170e17d76a92358777fb944ed23dcff98
Amalia <amalia.fergin@gmail.com>
2019-06-28 19:13:50 -0700

    pics are up and they are sliding

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import CarouselExample from './CarouselExample'
 import Carousel from 'react-responsive-carousel'
 
