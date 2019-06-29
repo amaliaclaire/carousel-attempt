1337124512a8abe0f1a06591b6b92fe87e015aba
Amalia <amalia.fergin@gmail.com>
2019-06-28 19:12:47 -0700

    pics are rendered shitty but they're up

--- src/components/CarouselExample.js
+++ src/components/CarouselExample.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Carousel } from "react-responsive-carousel";
 
 
 class CarouselExample extends React.Component {
 
   render () {
     return (
-      <div>Carousel is here</div>
+      <div>
+        <Carousel autoPlay>
+        <div>
+                  <img src="assets/1.jpeg" />
+                  <p className="legend">Legend 1</p>
+              </div>
+              <div>
+                  <img src="assets/2.jpeg" />
+                  <p className="legend">Legend 2</p>
+              </div>
+        </Carousel>
+      </div>
     )
   }
 }
