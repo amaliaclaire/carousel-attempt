b8f0ac7edc35f6447957aa61498654caf0f48356
Amalia <amalia.fergin@gmail.com>
2019-06-28 19:06:02 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import CarouselExample from './CarouselExample'
+import Carousel from 'react-responsive-carousel'
+
+
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <CarouselExample />
+        </div>
+      )
+    }
+}
+
+export default App;
---
+++ src/components/CarouselExample.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class CarouselExample extends React.Component {
+
+  render () {
+    return (
+      <div>Carousel is here</div>
+    )
+  }
+}
+
+
+export default CarouselExample;
